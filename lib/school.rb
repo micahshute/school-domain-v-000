4b6e3b2e507e9e2256aaea36720349da0d3639fb
Micah Shute <micah.shute@gmail.com>
2018-06-27 19:20:34 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+
+class School
+  attr_reader :name, :roster
+  attr_writer :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    puts "#{name}, #{grade}"
+    if @roster.keys.include?(grade)
+      puts "roster has grade"
+      @roster[grade] << name
+    else
+      puts "roster doesn't have grade"
+      @roster[grade] = [name]
+    end
+    
+  end
+end
+
+school = School.new("Bayside High School")
+school.add_student("bob", 9s)
+
+
